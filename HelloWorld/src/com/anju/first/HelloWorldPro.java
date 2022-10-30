07c227874b26ee7350eddedfb22bc518e928a7c4
anjanipriya456 <57087864+anjanipriya456@users.noreply.github.com>
2022-10-30 10:35:25 +0530

    First Commit

---
+++ HelloWorld/src/com/anju/first/HelloWorldPro.java
@@ -0,0 +1,11 @@
+package com.anju.first;
+
+public class HelloWorldPro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi World!!!");
+		System.out.println("This is Anju!!!");
+	}
+
+}
